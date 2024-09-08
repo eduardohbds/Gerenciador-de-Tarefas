715b04bc12610965063c2017f23672546b4728db
Eduardo Henrique <eduardo.hbds@gmail.com>
2024-09-07 22:52:43 -0300

    rafactor: add login and register to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
-  return <div>CodeGentitor</div>;
+  return(
+  <div className="container">
+    <div>App home</div>
+    <Register/>
+    <Login/>
+  </div>);
 };
 
 export default App;
